9f7b7f6372b27684dd82a219ae9863a91b23f48f
maheshdound <mahesh.dound@gmail.com>
2025-03-07 15:12:26 +0530

    cms file

---
+++ class management system.java
@@ -0,0 +1,12 @@
+class management system
+{
+int no of student=20;
+String types="higher secondery"
+String names="KLM"
+public static void main(String[] args)
+{
+System.out.println("display  management system");
+}
+
+}
+
